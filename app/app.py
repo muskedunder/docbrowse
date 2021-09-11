2e685e8f9ce8d71bfe969116a82ad1c47d6c7bf1
Colin Nordin <colin.nordin@gmail.com>
2021-09-11 17:16:54 +0200

    add document to db at app startup

--- app/app.py
+++ app/app.py
@@ -1,6 +1,22 @@
+from datetime import datetime
+
 from fastapi import FastAPI
 
+from . import database, models
+
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    doc1 = models.Document(
+        day=datetime(2021, 9, 11),
+        author="Stig Engstöm",
+        body="Jag lever.",
+    )
+    db = database.SessionLocal()
+    db.add(doc1)
+    db.commit()
+    db.close()
 
 @app.get("/")
 async def root():
