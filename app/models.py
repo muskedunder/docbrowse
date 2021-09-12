d91baf022b4c6b0f73a78d988244010cb179546b
Colin Nordin <colin.nordin@gmail.com>
2021-09-12 13:04:22 +0200

    create tsvector based on document body

--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 import uuid
 
-from sqlalchemy import Column, Date, LargeBinary, Text
+from sqlalchemy import cast, Column, Date, Index, LargeBinary, Text
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import func
 
 from .database import Base
+
+def create_tsvector(*args):
+    exp = args[0]
+    for e in args[1:]:
+        exp += ' ' + e
+    return func.to_tsvector('english', exp)
+
 
 class Document(Base):
     __tablename__ = "document"
@@ -11,3 +20,15 @@ class Document(Base):
     day = Column(Date, nullable=False)
     author = Column(Text, nullable=False)
     body = Column(Text, nullable=False)
+
+    __ts_vector__ = create_tsvector(
+        cast(func.coalesce(body, ''), postgresql.TEXT)
+    )
+
+    __table_args__ = (
+        Index(
+            'idx_body_fts',
+            __ts_vector__,
+            postgresql_using='gin'
+        ),
+    )
