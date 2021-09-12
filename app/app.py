c269847d158c228499f7348ce842febc205fb91c
Colin Nordin <colin.nordin@gmail.com>
2021-09-12 13:05:17 +0200

    add endpoints for adding and retrieving documents

--- app/app.py
+++ app/app.py
@@ -1,8 +1,13 @@
 from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.params import Depends
+from pydantic import BaseModel
+from sqlalchemy.orm.session import Session
 
 from . import database, models
+
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
@@ -18,6 +23,36 @@ def startup_event():
     db.commit()
     db.close()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+class Document(BaseModel):
+    author: str
+    date: str
+    body: str
+
+@app.post("/documents")
+async def create_document(document: Document, db: Session = Depends(database.get_db)):
+    doc = models.Document(
+        author=document.author,
+        day=document.date,
+        body=document.body,
+    )
+    db.add(doc)
+    db.commit()
+
+
+@app.get("/documents")
+async def get_documents(search_string: str, db: Session = Depends(database.get_db)):
+    documents = db.query(models.Document).filter(models.Document.__ts_vector__.match(search_string, postgresql_regconfig='english')).all()
+    documents = [
+        Document(
+            author=doc.author,
+            date=str(doc.day),
+            body=doc.body,
+        )
+        for doc in documents
+    ]
+    return documents
